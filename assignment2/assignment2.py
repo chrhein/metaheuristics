827f16530b359ce84c4f0496b3d266fe7e784b90
Christian Hein <scheiehein@gmail.com>
2020-01-23 14:59:22 +0100

    Created dataclass for Calls

--- assignment2/assignment2.py
+++ assignment2/assignment2.py
@@ -1,4 +1,5 @@
-from dataclasses import dataclass
+from dataclasses import dataclass, field
+from typing import List
 
 
 @dataclass
@@ -7,6 +8,20 @@ class Vehicle:
     home_node: int
     starting_time: int
     capacity: int
+    valid_calls: List[int] = field(default_factory=lambda: list())
+
+
+@dataclass
+class Call:
+    call_index: int
+    origin_node: int
+    destination_node: int
+    size: int
+    cost_no_transport: int
+    lb_tw_pu: int
+    ub_tw_pu: int
+    lb_tw_d: int
+    ub_tw_d: int
 
 
 with open("assets/Call_18_Vehicle_5.txt", "r") as f:
@@ -29,3 +44,24 @@ for _ in range(vehicles):
     strList = list(map(int, next(line).split(",")))
     vehicles_dict[strList[0]] = Vehicle(strList[0], strList[1], strList[2], strList[3])
 
+# number of calls
+next(line)
+calls = int(next(line))
+
+# for each vehicle, vehicle index, and then a list of calls that can be transported using that vehicle
+next(line)
+for _ in range(vehicles):
+    strList = list(map(int, next(line).split(",")))
+    vehicles_dict[strList[0]].valid_calls.extend(strList[1:])
+
+# for each call: call index, origin node, destination node, size, cost of not transporting, lb tw pu, ub tw pu,
+# lb tw d, ub tw d
+calls_dict = {}
+next(line)
+for _ in range(calls):
+    strList = list(map(int, next(line).split(",")))
+    calls_dict[strList[0]] = Call(strList[0], strList[1], strList[2], strList[3], strList[4], strList[5], strList[6],
+                                  strList[7], strList[8])
+
+# travel times and costs: vehicle, origin node, destination node, travel time (in hours), travel cost (in €)
+next(line)
