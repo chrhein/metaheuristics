70a14f843bd75f45aea0c71126fbf91914f5f070
Christian Hein <scheiehein@gmail.com>
2020-01-25 12:53:11 +0100

    Revert to old

--- assignment2/assignment2.py
+++ assignment2/assignment2.py
@@ -24,6 +24,15 @@ class Call:
     ub_tw_d: int
 
 
+@dataclass
+class Route:
+    vehicle_id: int
+    origin_node: int
+    destination_node: int
+    travel_time: int
+    travel_cost: int
+
+
 with open("assets/Call_18_Vehicle_5.txt", "r") as f:
     file = f.readlines()
 
@@ -41,8 +50,8 @@ vehicles = int(next(line))
 next(line)
 vehicles_dict = {}
 for _ in range(vehicles):
-    strList = list(map(int, next(line).split(",")))
-    vehicles_dict[strList[0]] = Vehicle(strList[0], strList[1], strList[2], strList[3])
+    string_list = list(map(int, next(line).split(",")))
+    vehicles_dict[string_list[0]] = Vehicle(string_list[0], string_list[1], string_list[2], string_list[3])
 
 # number of calls
 next(line)
@@ -51,17 +60,24 @@ calls = int(next(line))
 # for each vehicle, vehicle index, and then a list of calls that can be transported using that vehicle
 next(line)
 for _ in range(vehicles):
-    strList = list(map(int, next(line).split(",")))
-    vehicles_dict[strList[0]].valid_calls.extend(strList[1:])
+    string_list = list(map(int, next(line).split(",")))
+    vehicles_dict[string_list[0]].valid_calls.extend(string_list[1:])
 
 # for each call: call index, origin node, destination node, size, cost of not transporting, lb tw pu, ub tw pu,
 # lb tw d, ub tw d
 calls_dict = {}
 next(line)
 for _ in range(calls):
-    strList = list(map(int, next(line).split(",")))
-    calls_dict[strList[0]] = Call(strList[0], strList[1], strList[2], strList[3], strList[4], strList[5], strList[6],
-                                  strList[7], strList[8])
+    string_list = list(map(int, next(line).split(",")))
+    calls_dict[string_list[0]] = Call(string_list[0], string_list[1], string_list[2], string_list[3], string_list[4],
+                                      string_list[5], string_list[6], string_list[7], string_list[8])
 
 # travel times and costs: vehicle, origin node, destination node, travel time (in hours), travel cost (in €)
+routes_dict = {}
 next(line)
+for _ in range(vehicles * nodes * nodes):
+    string_list = list(map(int, next(line).split(",")))
+    print(string_list)
+    routes_dict[string_list[0]] = Route(string_list[0], string_list[1], string_list[2], string_list[3], string_list[4])
+
+print(routes_dict)
