cce8f15a3561af7f883c4faa70d1636054dbd211
Christian Hein <scheiehein@gmail.com>
2020-03-26 16:36:17 +0100

    2-exchange is working

--- assignment3/search_algorithms/local_search.py
+++ assignment3/search_algorithms/local_search.py
@@ -5,22 +5,49 @@ from cost_calculation import f
 from veri import check_solution
 
 
+def get_calls(solution):
+    calls = {}
+    vehicle_calls = []
+    vehicle_index = 1
+    for call in solution:
+        if call == 0:
+            calls[vehicle_index] = vehicle_calls
+            vehicle_index += 1
+            vehicle_calls = []
+        else:
+            vehicle_calls.append(call)
+    calls[vehicle_index] = vehicle_calls
+    return calls
+
+
 def two_exchange(solution):
-    old_sol = solution.copy()
-    new_sol = old_sol.copy()
+    calls = get_calls(solution)
+    rand_ub = x.vehicles + 2
+    rand = random.randrange(1, rand_ub)
+    two_exchange_list = calls.get(rand)
+    if not two_exchange_list or len(two_exchange_list) == 2:
+        return solution
+    else:
+        rand1 = random.choice(two_exchange_list)
+        rand2 = random.choice(two_exchange_list)
+        while rand1 == rand2:
+            rand2 = random.choice(two_exchange_list)
+        rand1_indexes = get_index_positions(two_exchange_list, rand1)
+        rand2_indexes = get_index_positions(two_exchange_list, rand2)
 
-    c = x.calls
-    rand1 = random.randrange(1, c)
-    rand2 = random.randrange(1, c)
-    while rand2 == rand1:
-        rand2 = random.randrange(1, c)
-
-    index_rand1 = old_sol.index(rand1)
-    index_rand2 = old_sol.index(rand2)
-
-    new_sol[index_rand1] = old_sol[index_rand2]
-    new_sol[index_rand2] = old_sol[index_rand1]
-    return new_sol
+        two_exchange_list[rand1_indexes[0]], two_exchange_list[rand2_indexes[0]] = \
+            two_exchange_list[rand2_indexes[0]], two_exchange_list[rand1_indexes[0]]
+        two_exchange_list[rand1_indexes[1]], two_exchange_list[rand2_indexes[1]] = \
+            two_exchange_list[rand2_indexes[1]], two_exchange_list[rand1_indexes[1]]
+        calls[rand] = two_exchange_list
+        new_solution = []
+        for val in calls.values():
+            if not val:
+                new_solution.append(0)
+            else:
+                for call in val:
+                    new_solution.append(call)
+    return new_solution
 
 
 def three_exchange(solution):
@@ -72,3 +99,21 @@ def local_search(init_solution):
             best_solution = current
 
     return best_solution
+
+
+def get_index_positions(list_of_elements, element):
+    ''' Returns the indexes of all occurrences of give element in
+    the list- listOfElements '''
+    index_pos_list = []
+    index_pos = 0
+    while True:
+        try:
+            # Search for item in list from index_pos to the end of list
+            index_pos = list_of_elements.index(element, index_pos)
+            # Add the index position in list
+            index_pos_list.append(index_pos)
+            index_pos += 1
+        except ValueError as e:
+            break
+
+    return index_pos_list
