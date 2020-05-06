c5d4cc532d1665348f58ed139addf3b0adc616a4
Christian Hein <scheiehein@gmail.com>
2020-05-06 15:52:19 +0200

    Updated before new branch

--- inf273_main_assignment/main.py
+++ inf273_main_assignment/main.py
@@ -8,8 +8,8 @@ from initializers.simulated_annealing_new_init import new_simulated_annealing_in
 
 def main():
     init_solution = solution_generator()
-    times = 10
-    runtime = 20
+    times = 5
+    runtime = 120
     r = runtime - 0.1
     # random_solution_initializer(init_solution, times)
     # local_search_initializer(init_solution, times)
